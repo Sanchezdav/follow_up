0902e57aee21d97a330ea6fd79b89ba96f22414c
David <sanchez.dav90@gmail.com>
2020-10-22 21:41:02 -0500

    Add multilple emails to invites

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -2,25 +2,28 @@ class InvitesController < ApplicationController
   before_action :set_project, only: :create
 
   def create
-    @invite = @project.invites.new(invite_params)
-    @invite.sender = current_user
-
-    if @invite.save
-      if @invite.recipient
-        ProjectInviteMailer.with(invite: @invite).existing_user_invite.deliver_later
-        @invite.recipient.collaborations.push(@invite.project)
-        redirect_to @project, notice: "#{@invite.recipient.email} was added to the project."
-      else
-        ProjectInviteMailer
-          .with(
-            invite: @invite,
-            url: new_user_registration_url(invite_token: @invite.token)
-          )
-          .new_user_invite
-          .deliver_later
-        redirect_to @project, notice: "#{@invite.email} was invited to the project."
+    emails = invite_params[:emails].split(', ')
+    emails.each do |email|
+      @invite = @project.invites.new(email: email)
+      @invite.sender = current_user
+  
+      if @invite.save
+        if @invite.recipient
+          ProjectInviteMailer.with(invite: @invite).existing_user_invite.deliver_later
+          @invite.recipient.collaborations.push(@invite.project)
+        else
+          ProjectInviteMailer
+            .with(
+              invite: @invite,
+              url: new_user_registration_url(invite_token: @invite.token)
+            )
+            .new_user_invite
+            .deliver_later
+        end
       end
     end
+
+    redirect_to @project, notice: 'Invitations were sent successfully'
   end
 
   private
@@ -30,6 +33,6 @@ class InvitesController < ApplicationController
   end
 
   def invite_params
-    params.require(:invite).permit(:email)
+    params.require(:invite).permit(:emails)
   end
 end
